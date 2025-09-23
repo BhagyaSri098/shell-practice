bf0be9c82833f497ca32363a9f87bfceb7353fd5
bhagyasri <bhagyasriof098@gmail.com>
2025-09-23 15:44:06 +0530

    pin invisible

--- 06-pin.sh
+++ 06-pin.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Please enter your PIN number"
-read PIN_NUMBER #here pin number is variable
+read -s PIN_NUMBER #here pin number is variable
 
 echo "PIN entered is: $PIN_NUMBER"
+

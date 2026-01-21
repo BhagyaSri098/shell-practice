066908ce665eb1890e3f1520fd9f2d628afb85d0
bhagyasri <bhagyasriof098@gmail.com>
2026-01-21 14:11:44 +0530

    conversation

---
+++ 02-conv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi suresh , how are you?"
+echo "Suresh:: Hello ramesh, iam fine. how are you?"
+echo "ramesh:: what are you learning recently?"
+echo "suresh:: iam learning shell scripting ramesh"
